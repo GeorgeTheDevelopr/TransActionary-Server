4cd7afd251d750faea0d4d794830295443ea759a
George Brown <sumthnabtgeorge@gmail.com>
2020-09-25 19:04:28 -0400

    fixed arrangement

--- migrations/002.do.create_items.sql
+++ migrations/002.do.create_items.sql
@@ -3,7 +3,5 @@ CREATE TABLE IF NOT EXISTS items (
   vendor TEXT NOT NULL,
   items TEXT NOT NULL,
   full_price TEXT NOT NULL,
-  alt_id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  alt_id2 uuid NOT NULL DEFAULT uuid_generate_v4(),
   owner_id INT REFERENCES users(id) ON DELETE CASCADE
 );
